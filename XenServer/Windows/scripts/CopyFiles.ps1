e719d4eafc1fd0068722d32882e7309eef294690
Francisco Arencibia <francisco.arencibia@jotelulu.com>
2024-09-06 10:26:57 +0200

    Adding Windows 2022 templates for XenServer

---
+++ XenServer/Windows/scripts/CopyFiles.ps1
@@ -0,0 +1,33 @@
+# Definir las rutas de origen y destino intermedias
+$srcDir = 'A:\'
+$tempDir = 'C:\apps\'
+
+$dstSysprepDir = 'C:\Windows\System32\Sysprep\'
+
+# Crear directorio temporal si no existe
+if (-not (Test-Path $tempDir)) {
+    New-Item -Path $tempDir -ItemType Directory -Force
+}
+
+# Copiar todo desde A:\ a C:\apps
+Copy-Item -Path $srcDir* -Destination $tempDir -Recurse -Force
+
+Write-Host 'Archivos copiados temporalmente a C:\apps.'
+
+# Comprobar que el archivo de Sysprep existe antes de copiar a destino final
+$src2k22unattend = Join-Path $tempDir '2k22unattend.xml'
+
+if (-not (Test-Path $src2k22unattend)) {
+    Write-Host '2k22unattend.xml no encontrado en C:\apps'
+    exit 1
+}
+
+# Crear el directorio de destino final si no existe
+if (-not (Test-Path $dstSysprepDir)) {
+    New-Item -Path $dstSysprepDir -ItemType Directory -Force
+}
+
+# Copiar el archivo 2k22ESunattend.xml desde C:\apps al directorio de destino final
+Copy-Item -Path $src2k22unattend -Destination $dstSysprepDir -Force
+
+Write-Host 'Archivo 2k22unattend.xml copiado correctamente a su destino final.'
